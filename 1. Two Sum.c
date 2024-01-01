27e35240ee061ded11ea941f0168e80298c10071
Tran Dinh Dung <114976992+trandung261o@users.noreply.github.com>
2024-01-02 05:29:22 +0700

    Update 1. Two Sum.c

--- 1. Two Sum.c
+++ 1. Two Sum.c
@@ -6,8 +6,7 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     *returnSize = 2;
     for (int i = 0; i < numsSize-1; i++) {
         for (int j = i+1; j < numsSize; j++) {
-            int sum = nums[i] + nums[j];
-            if (sum == target) {
+            if (nums[i] + nums[j] == target) {
                 res[0] = i; res[1] = j;
                 return res;
             }
